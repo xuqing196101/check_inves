a8cea8a6fcc02371fc43b6b8a36388a5645425f0
lxxmm <944870955@qq.com>
2016-09-01 15:10:04 +0800

    日期全局转换器

---
+++ src/main/java/yggc/util/DateConvert.java
@@ -0,0 +1,22 @@
+package yggc.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.context.request.WebRequest;
+
+public class DateConvert implements WebBindingInitializer{
+
+	@Override
+	public void initBinder(WebDataBinder binder, WebRequest request) {
+		// <3> 定义转换格式
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				//true代表允许输入值为空
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+		
+	}
+
+}
---
+++ src/main/java/yggc/util/DateUtil.java
@@ -0,0 +1,26 @@
+package yggc.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+
+public class DateUtil implements Converter<String, Date> {
+
+	@Override
+	public Date convert(String source) {
+		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");      
+	        dateFormat.setLenient(false);      
+	        try {      
+	            return dateFormat.parse(source);      
+	        } catch (Exception e) {      
+	            e.printStackTrace();      
+	        }   
+	        
+		return null;
+	}
+
+	 
+
+}
