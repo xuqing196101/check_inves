003f2a49ff0b718cfde78e48bd39af7d1472b2d1
lxxmm <944870955@qq.com>
2016-09-13 17:12:59 +0800

    修改错误

--- src/main/java/ses/test/Demo.java
+++ src/main/java/ses/test/Demo.java
@@ -1,12 +1,19 @@
 package ses.test;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ses.dao.bms.UserMapper;
 import ses.model.bms.User;
+import ses.model.bms.UserTask;
 import ses.service.bms.UserServiceI;
+import ses.service.bms.UserTaksService;
 
 
 public class Demo extends BaseTest {
@@ -16,18 +23,33 @@ public class Demo extends BaseTest {
 	
 	@Autowired
 	private UserServiceI userServiceI;
+	
+	@Autowired
+	private UserTaksService userTaksService;
 	@Test
 	public void test1(){
 
-		User user = userMapper.queryById("154");
-		System.out.println(user.getLoginName()+"-----");
+//		User user = userMapper.queryById("154");
+//		System.out.println(user.getLoginName()+"-----");
 	}
 	@Test
 	public void test2(){
-		User user =new User ();
-		user.setId("154");
-		User user2 = userServiceI.getUserById(user);
-		
-		System.out.println(user2.getLoginName()+"*****************************");
+		 UserTask u=new UserTask();
+		 u.setId("1");
+		 Date date = new Date();
+		 System.out.println(date);
+		 u.setCreatedAt(date);
+		 
+		 userTaksService.add(u);
+	}
+	@Test
+	public void testDate() throws ParseException{
+//		  String s = "Sun Sep 02 2012 08:30:00 GMT+08:00";
+		  String str="Sat Sep 10 2016 06:00:00 GMT+0800";
+		  String[] strs= str.split("\\+");
+		 String ss= strs[0]+"+08:00";
+//	        SimpleDateFormat sf = new SimpleDateFormat("EEE MMM dd yyyy hh:mm:ss z",Locale.ENGLISH);
+//	        Date d = sf.parse(s);
+		System.out.println(ss);
 	}
 }
