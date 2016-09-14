f31034e97b84980d702f6db8b999920b1d3ab3aa
lxxmm <944870955@qq.com>
2016-09-14 15:56:55 +0800

    controller

---
+++ src/main/java/bss/controller/base/BaseController.java
@@ -0,0 +1,12 @@
+package bss.controller.base;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+public class BaseController {
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+}
