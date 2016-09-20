516c556366208da7be0c9afd53d24515519701d7
zxfjava <782458486@qq.com>
2016-09-20 08:53:17 +0800

    产品参数管理

---
+++ src/main/java/ses/model/prms/ProductParamValue.java
@@ -0,0 +1,24 @@
+package ses.model.prms;
+
+public class ProductParamValue {
+	/**
+	 * id
+	 * 
+	 * */
+	private String id;
+	/**
+	 * 参数id
+	 * 
+	 * */
+	private ProductParam productParam;
+	/**
+	 * 参数值
+	 * 
+	 * 
+	 * */
+	private String paramValue;
+	
+	
+	
+	
+}
