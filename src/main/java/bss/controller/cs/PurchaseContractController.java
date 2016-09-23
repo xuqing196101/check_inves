90a71cb15e46e62c89193c3764abad99c63f9c2e
MRlovablee <2232158164@qq.com>
2016-09-23 17:23:21 +0800

    根据合同编号查询合同controller实现

--- src/main/java/bss/controller/cs/PurchaseContractController.java
+++ src/main/java/bss/controller/cs/PurchaseContractController.java
@@ -1,15 +1,14 @@
 package bss.controller.cs;
 
 import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 import bss.model.cs.PurchaseContract;
 import bss.service.cs.PurchaseContractService;
-
 
 /* 
  *@Title:PurchaseContractController
@@ -25,9 +24,18 @@ public class PurchaseContractController {
 	@Autowired
 	private PurchaseContractService purchaseContractService;
 	
+	@SuppressWarnings("unused")
 	@RequestMapping("/selectAllPuCon")
 	public String selectAllPurchaseContract() throws Exception{
 		List<PurchaseContract> conList = purchaseContractService.selectAllPurchaseContract();
 		return null;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/selectByCode")
+	public PurchaseContract selectByCode(HttpServletRequest request) throws Exception{
+		String code = request.getParameter("code");
+		PurchaseContract purchaseCon = purchaseContractService.selectByCode(code);
+		return purchaseCon;
 	}
 }
