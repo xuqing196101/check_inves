1330b0d9645be0b543705a9e40c535574d3c69e9
Whuijie <wanghuijie0510@163.com>
2016-12-28 16:44:55 +0800

    【修改】修改报错【作者】王慧杰

---
+++ src/test/java/synchro/task/TestExpertReadTask.java
@@ -0,0 +1,34 @@
+package synchro.task;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import synchro.inner.read.FilesRepeater;
+
+/**
+ * 
+ * 版权：(C) 版权所有 
+ * <简述> 专家读取测试
+ * <详细描述>
+ * @author   WangHuijie
+ * @version  
+ * @since
+ * @see
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations ={"classpath*:spring.xml","classpath*:spring-mvc.xml",
+                                  "classpath*:spring-mybatis.xml"})
+public class TestExpertReadTask {
+
+    @Resource
+    private FilesRepeater fileRepeater;
+    
+    @Test
+    public void test(){
+        fileRepeater.initFiles();
+    }
+}
