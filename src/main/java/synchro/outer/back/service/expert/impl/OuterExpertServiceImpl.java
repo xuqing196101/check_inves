c532e98053aa0626f1163dcd37f96dbccf417441
Whuijie <wanghuijie0510@163.com>
2016-12-28 16:46:22 +0800

    【修改】解决报错【作者】王慧杰

--- src/main/java/synchro/outer/back/service/expert/impl/OuterExpertServiceImpl.java
+++ src/main/java/synchro/outer/back/service/expert/impl/OuterExpertServiceImpl.java
@@ -3,7 +3,6 @@ package synchro.outer.back.service.expert.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
