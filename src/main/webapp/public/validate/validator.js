5a2d6ecd226ab1b8d396685f80af186fd9e1e9f3
Whuijie <wanghuijie0510@163.com>
2017-01-20 15:47:36 +0800

    【修改】修改报错【作者】王慧杰

--- src/main/webapp/public/validate/validator.js
+++ src/main/webapp/public/validate/validator.js
@@ -48,5 +48,5 @@ jQuery.validator.addMethod("isFax", function(value, element){
 // 网址验证
 jQuery.validator.addMethod("isUrl", function(value, element) { 
 	var url = /^((https?|ftp|news):\/\/)?([a-z]([a-z0-9\-]*[\.。])+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&]*)?)?(#[a-z][a-z0-9_]*)?$/; 
-	return this.optional(element) || (url.test(value);
+	return this.optional(element) || (url.test(value));
 }, "请输入正确的网址"); 
