cd59f7573647d837ce3047fafc2c8e15701e5568
lxxmm <944870955@qq.com>
2017-02-23 10:13:44 +0800

    供应商资质文件方法【作者 lixiaoixao

---
+++ src/main/java/ses/service/bms/QualificationLevelService.java
@@ -0,0 +1,21 @@
+package ses.service.bms;
+
+
+import java.util.List;
+
+import ses.model.bms.DictionaryData;
+import ses.model.bms.QualificationLevel;
+
+/**
+ * Created by yggc on 2017/2/22.
+ */
+public interface QualificationLevelService {
+	
+    public List<QualificationLevel> findList(Integer pageNum, String qualificationId );
+
+    public void save(String id,String qualificationId,String grade);
+
+
+    
+    public List<DictionaryData> getByQuaId(String id);
+}
