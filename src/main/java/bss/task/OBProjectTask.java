58f0366082b469efd9f6d2a01dde7b8564111450
wjsnypm <wjsnypm@126.com>
2017-03-13 16:12:48 +0800

    【修改】修改冲突【作者杨红亮】

---
+++ src/main/java/bss/task/OBProjectTask.java
@@ -0,0 +1,27 @@
+package bss.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import bss.service.ob.OBProjectServer;
+
+/**
+ * <简述>
+ * 定时任务:处理竞价 状态 和供应商之间的业务逻辑
+ * <详细描述>
+ * @author  YangHongLiang
+ * @version  
+ */
+@Component("OBProjectTask")
+public class OBProjectTask {
+	/**竞价信息服务**/
+	@Autowired
+    private OBProjectServer OBProjectServer;
+	/***
+	 * 定时处理 竞价信息 业务逻辑
+	 */
+	public void handleOBProject(){
+		System.out.println("ddddd  ddd"+System.currentTimeMillis());
+		OBProjectServer.changeStatus();
+	}
+}
