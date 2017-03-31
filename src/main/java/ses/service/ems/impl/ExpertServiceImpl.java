b8f134d90d69e6c87fe801384e4617b4cebff21d
lxxmm <944870955@qq.com>
2017-03-31 11:22:38 +0800

    IE下载专家申请表打不开【作者】李晓晓

--- src/main/java/ses/service/ems/impl/ExpertServiceImpl.java
+++ src/main/java/ses/service/ems/impl/ExpertServiceImpl.java
@@ -283,7 +283,7 @@ public class ExpertServiceImpl implements ExpertService {
 			    HttpHeaders headers = new HttpHeaders(); 
 			    headers.setContentDispositionFormData("attachment", downFileName);   
 			    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);   
-			    ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.CREATED); 
+			    ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.OK); 
 			    file.delete();
 			    return entity;
 		} catch (Exception e) {
