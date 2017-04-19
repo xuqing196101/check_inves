461fa55cdef32c6f14d4025997891e632d2df043
wjsnypm <wjsnypm@126.com>
2017-04-19 11:43:44 +0800

    【添加】修改竞价导出导入【作者】杨红亮

--- src/main/java/synchro/task/inner/importTask/InfoImportTask.java
+++ src/main/java/synchro/task/inner/importTask/InfoImportTask.java
@@ -1,9 +1,22 @@
 package synchro.task.inner.importTask;
 
+import java.io.File;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import bss.service.ob.OBProjectServer;
+
+import ses.model.bms.DictionaryData;
+import ses.util.DictionaryDataUtil;
 import synchro.inner.read.InnerFilesRepeater;
+import synchro.service.SynchRecordService;
+import synchro.util.Constant;
+import synchro.util.DateUtils;
+import synchro.util.FileUtils;
+import synchro.util.OperAttachment;
 
 /**
  * 
@@ -24,7 +37,9 @@ public class InfoImportTask {
      */
     @Autowired
     private InnerFilesRepeater fileRepeater;
-    
+    /**竞价信息**/
+    @Autowired
+    private OBProjectServer OBProjectServer;
     /**
      * 
      *〈简述〉任务
@@ -34,6 +49,29 @@ public class InfoImportTask {
     public void innerInfoImportTask(){
         fileRepeater.initFiles();
         /**内网导入**/
+        File file = FileUtils.getImportFile();
+        DictionaryData result = DictionaryDataUtil.get(Constant.DATA_TYPE_BIDDING_RESULT_CODE);
+        if(result!=null && StringUtils.isNotBlank(result.getId())){
+        	/**竞价结果导出  只能是外网导入内网**/
+        	if (file != null && file.exists()){
+                 File [] files = file.listFiles();
+                 for (File f : files){
+                	  if (f.isDirectory()){
+                          if (f.getName().equals(Constant.RESULT_FILE_EXPERT)){
+                        	  for (File file2 : f.listFiles()) {
+                        		  if (file2.getName().contains(FileUtils.C_OB_PROJECT_RESULT_FILENAME)){
+                        			  OBProjectServer.importProjectResult(file2);
+                        		  }
+                        	  }
+                          }
+                	  }
+                     if (f.isDirectory()){
+                         if (f.getName().equals(Constant.RESULT_FILE_EXPERT)){
+                             OperAttachment.moveFolder(f);
+                         }
+                     }
+                 }
+             }
+        }
     }
-    
 }
