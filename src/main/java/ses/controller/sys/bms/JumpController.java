abc7681e2ea0c5892146ee9517b3e4efbe2908ab
changsongyang <y_ch_s@163.com>
2017-05-23 20:04:09 +0800

    【修改】修改支撑环境  专家管理 数据字典 专家管理 面包屑链接（点击打开新窗口）

---
+++ src/main/java/ses/controller/sys/bms/JumpController.java
@@ -0,0 +1,40 @@
+package ses.controller.sys.bms;
+
+import common.annotation.CurrentUser;
+import common.annotation.SystemControllerLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ses.model.bms.Analyze;
+import ses.model.bms.AnalyzeItem;
+import ses.model.bms.User;
+import ses.service.ems.AnalyzeService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+* @ClassName: AnalyzeController 
+* @Description: 后台统计控制类
+* @author Easong
+* @date 2017年5月3日 下午2:55:48 
+*
+ */
+@Controller
+@RequestMapping("/jumppage")
+public class JumpController {
+	
+	/**
+	 * 
+	* @Description: 进入页面跳转
+	 */
+	@RequestMapping("/jump")
+	public String to(Model model){
+
+		return "jumpPage";
+	}
+
+}
