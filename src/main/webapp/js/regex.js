b7977bec8a171c6acc3c5f5418045bce488ce720
githubhxg <1059056687@qq.com>
2017-06-06 15:48:07 +0800

    【修改】供应商注册--供应商类型技术人员数量比例输入格式的校验【作者】黄习刚

--- src/main/webapp/js/regex.js
+++ src/main/webapp/js/regex.js
@@ -11,7 +11,7 @@ function validateMoney(val) {
 
 // 验证百分比
 function validatePercentage(val) {
-	var reg = /^(([1-9]\d{1}|0|100)(\.\d{1,2})?%)?$/;
+	var reg = /^(([1-9]\d{0,1}|0|100)(\.\d{1,2})?%)?$/;
 	if (!reg.test(val)) {
 		//alert("百分比格式不对，正确格式为0%-100%，最多两位小数！");
 		layer.msg("百分比格式不对，正确格式为0%-100%，最多两位小数！");
@@ -22,7 +22,7 @@ function validatePercentage(val) {
 
 // 验证百分比
 function validatePercentage2(val) {
-	var reg = /^(([1-9]\d{1}|0|100)(\.\d{1,2})?)?$/;
+	var reg = /^(([1-9]\d{0,1}|0|100)(\.\d{1,2})?)?$/;
 	if (!reg.test(val)) {
 		//alert("百分比格式不对，正确格式为0-100的数字，最多两位小数！");
 		layer.msg("百分比格式不对，正确格式为0-100的数字，最多两位小数！");
