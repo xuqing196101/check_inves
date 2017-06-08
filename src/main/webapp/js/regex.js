9b1f5d11cd5604746d8f3c71c3d962fbc1433037
githubhxg <1059056687@qq.com>
2017-06-08 17:54:48 +0800

    【修改】供应商注册--质量体系认证证书没有带出来的问题【作者】黄习刚

--- src/main/webapp/js/regex.js
+++ src/main/webapp/js/regex.js
@@ -43,3 +43,45 @@ function validatePercentageSupplier(val,id) {
 	}
 	return true;
 }
+
+//验证手机号
+function validateMobile(val) {
+	var reg = /^(1[3|4|5|8][0-9]\d{4,8})?$/gi;
+	if (!reg.test(val)) {
+		layer.msg("手机号格式不对！例如：13800000000");
+		return false;
+	}
+	return true;
+}
+
+//验证电话和手机
+function validateMobileAndTel(val){
+	var regMob = /^(1[3|4|5|8][0-9]\d{4,8})?$/;
+    var regTel = /^(([0-9]{3,4}-)?[0-9]{7,8})?$/;
+    if(regMob.test(val) || regTel.test(val)){
+        return true;
+    } else {
+    	layer.msg("联系电话格式不对！手机：13800000000，固话：010-12345678或0310-1234567");
+        return false;
+    }
+}
+
+//验证电话号码格式
+function chkTelPhone(obj) {
+    var regTel = /^(([0-9]{3,4}-)?[0-9]{7,8})?$/;
+    if(!regTel.test(val)){
+    	layer.msg("固话格式不对！例如：010-12345678或0310-1234567");
+        return false;
+    }
+    return true;
+}
+
+//验证Email
+function validateEmail(val) {
+	var reg = /^([a-zA-Z]+[_|\-|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\-|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/gi;
+	if(!reg.test(val)){
+		layer.msg("电子邮箱格式不对！例如：123456@qq.com");
+        return false;
+	}
+	return true;
+}
