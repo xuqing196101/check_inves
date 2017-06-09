547c92d717834b418121cbe7972886e24bea18ba
easong27 <easong27@163.com>
2017-06-09 10:15:50 +0800

    【添加】采购资源综合展示-采购计划展示

--- src/main/webapp/js/dss/rids/analyze/analyzePurchasePlan.js
+++ src/main/webapp/js/dss/rids/analyze/analyzePurchasePlan.js
@@ -44,7 +44,6 @@ $(function() {
 		} ],
 		 series: [
 		          {
-		        	  name: "下达采购计划总金额",
 		              itemStyle: {
 		                  normal: {
 		                      color: function(params) {
@@ -80,12 +79,116 @@ $(function() {
 		      
 	};
 	
+	optionPlan1 = {
+			title : {
+				top : 0,
+				left : '43%'
+			},
+			tooltip : {
+				trigger: 'axis'
+			},
+			legend : {
+				bottom : 0
+			},
+			toolbox : {
+				show : true,
+				feature : {
+					dataView : {
+						show : true,
+						readOnly : false
+					},
+					magicType : {
+						show : true,
+						type : [ 'line', 'bar' ]
+					},
+					restore : {
+						show : true
+					},
+					saveAsImage : {
+						show : true
+					}
+				},
+				dataView: {show: true, readOnly: true}, 
+			},
+			calculable : true,
+			xAxis  : [ {
+				axisLabel:{interval: 0,
+					formatter:function(val){
+						return val.split("").join("\n");
+					}
+				}
+			} ],
+			series: [
+			         {
+			        	 name: "总金额",
+			        	 itemStyle: {
+			        		 normal: {
+			        			 color: function(params) {
+			        				 // build a color map as your need.
+			        				 var colorList = [
+			        				                  '#C1232B','#B5C334','#FCCE10','#E87C25','#27727B',
+			        				                  '#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD',
+			        				                  '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
+			        				                  ];
+			        				 return colorList[params.dataIndex]
+			        			 },
+			        			 label: {
+			        				 show: true,
+			        				 position: 'top',
+			        				 formatter: '{b}\n{c}'
+			        			 }
+			        		 }
+			        	 },
+			        	 markPoint: {
+			        		 tooltip: {
+			        			 trigger: 'item',
+			        			 backgroundColor: 'rgba(0,0,0,0)',
+			        			 formatter: function(params){
+			        				 return '<img src="' 
+			        				 + params.data.symbol.replace('image://', '')
+			        				 + '"/>';
+			        			 }
+			        		 },
+			        		 
+			        	 }
+			         }
+			         ]
+			
+	};
+	
+	// 近5年下达采购计划批次和金额
+	$.ajax({
+		url : globalPath + "/resAnalyze/selectNowFiveYearAllBudgetByPlan.do",
+		type : "POST", // 请求方式
+		dataType : "json", // 返回格式为json
+		success : function(data) {
+			$("#nowFiveYearPurPlanBudgetBYOrg").echartsTemplate({
+				dataList:data,
+				XField:'name',
+				YField:'value',
+				groupField:'group',
+				chartType:'bar',
+				XTitle:'年份',
+				YTitle:'金额&批次',
+				customEchartsOptions:function(res,dataList,option){
+					return optionPlan;
+				}
+			});
+			// 获取echart对应的对象
+			var supplierOrg = $("#nowFiveYearPurPlanBudgetBYOrg").echartsTemplate("getMyChart", null);
+			supplierOrg.on('click', function(params) {
+				window.location.href = globalPath + "/purchase/list.html?reqType=analyze&purchaseDepShortName="+params.data.id;
+			});
+		}
+	});
+	
+	// 各采购管理部门下达采购计划总金额
 	$.ajax({
 		url : globalPath + "/resAnalyze/selectManageBudget.do",
 		type : "POST", // 请求方式
 		dataType : "json", // 返回格式为json
 		success : function(data) {
-			$("#purPlanBudgetBYOrg").echartsTemplate({
+			$("#purPlanBudgetByManageOrg").echartsTemplate({
 				dataList:data,
 				XField:'name',
 				YField:'value',
@@ -94,11 +197,37 @@ $(function() {
 				XTitle:'部门',
 				YTitle:'金额/万元',
 				customEchartsOptions:function(res,dataList,option){
-					return optionPlan;
+					return optionPlan1;
 				}
 			});
 			// 获取echart对应的对象
-			var supplierOrg = $("#purPlanBudgetBYOrg").echartsTemplate("getMyChart", null);
+			var supplierOrg = $("#purPlanBudgetByManageOrg").echartsTemplate("getMyChart", null);
+			supplierOrg.on('click', function(params) {
+				window.location.href = globalPath + "/purchase/list.html?reqType=analyze&purchaseDepShortName="+params.data.id;
+			});
+		}
+	});
+	
+	// 采购机构获取前10名的总金额
+	$.ajax({
+		url : globalPath + "/resAnalyze/selectPlanBudget.do",
+		type : "POST", // 请求方式
+		dataType : "json", // 返回格式为json
+		success : function(data) {
+			$("#purPlanBudgetByOrg").echartsTemplate({
+				dataList:data,
+				XField:'name',
+				YField:'value',
+				groupField:'group',
+				chartType:'bar',
+				XTitle:'部门',
+				YTitle:'金额/万元',
+				customEchartsOptions:function(res,dataList,option){
+					return optionPlan1;
+				}
+			});
+			// 获取echart对应的对象
+			var supplierOrg = $("#purPlanBudgetByOrg").echartsTemplate("getMyChart", null);
 			supplierOrg.on('click', function(params) {
 				window.location.href = globalPath + "/purchase/list.html?reqType=analyze&purchaseDepShortName="+params.data.id;
 			});
