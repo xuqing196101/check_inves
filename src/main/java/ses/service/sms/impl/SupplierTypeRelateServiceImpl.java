adb090c511dfdd47d7d75461e6b6e7532e555e55
wjsnypm <wjsnypm@126.com>
2017-07-31 10:40:25 +0800

    【修改】修改供应商类型显示【作者】杨红亮

--- src/main/java/ses/service/sms/impl/SupplierTypeRelateServiceImpl.java
+++ src/main/java/ses/service/sms/impl/SupplierTypeRelateServiceImpl.java
@@ -163,4 +163,8 @@ public class SupplierTypeRelateServiceImpl implements SupplierTypeRelateService 
 		
 	}
 
+	@Override
+	public List<String> findTypeBySupplierId(String supplierId) {
+		return supplierTypeRelateMapper.findTypeBySupplierId(supplierId);
+	}
 }
