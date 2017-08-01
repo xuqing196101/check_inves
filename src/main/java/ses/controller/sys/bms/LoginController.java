dcf41ce86e4641c2fb6f5b6e895f8d2d46ef17c1
wjsnypm <wjsnypm@126.com>
2017-08-01 18:00:30 +0800

    【添加】添加供应商退回再审核状态/添加查询【作者】杨红亮

--- src/main/java/ses/controller/sys/bms/LoginController.java
+++ src/main/java/ses/controller/sys/bms/LoginController.java
@@ -264,8 +264,10 @@ public class LoginController {
                             } else if (("publicity").equals(msg)){
                                 // 公示中状态
                                 out.print("publicity");
-
-                          }
+                            }else if("send_back".equals(msg)){
+                            	//退回再审核
+                            	out.print("send_back,"+ u.getId());
+                            }
 //                        }else if(0 < validateDay){//未按规定时间提交审核,注销信息
 //                            out.print("supplier_logout," + validateDay);
 //                        }
