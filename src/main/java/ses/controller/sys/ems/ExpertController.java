7d49944ad221fb8630443b4c3be6823c2088b2b2
wjsnypm <wjsnypm@126.com>
2017-08-17 14:32:01 +0800

    【修改】修改供应商复审下载入库申请表报错【作者】杨红亮

--- src/main/java/ses/controller/sys/ems/ExpertController.java
+++ src/main/java/ses/controller/sys/ems/ExpertController.java
@@ -2959,6 +2959,9 @@ public class ExpertController extends BaseController {
         // 生产经营地址
         List < SupplierAddress > addressList = supplier.getAddressList();
         for(SupplierAddress address: addressList) {
+            if(StringUtils.isBlank(address.getAddress())){
+                continue;
+            }
             Area addr = areaServiceI.listById(address.getAddress());
             if(addr != null) {
                 String province = areaServiceI.listById(addr.getParentId()).getName();
@@ -2973,7 +2976,10 @@ public class ExpertController extends BaseController {
             for(SupplierBranch branch: branchList) {
                 // 国家(地区)
                 if(branch.getCountry() != null) {
-                    branch.setCountry(DictionaryDataUtil.findById(branch.getCountry()).getName());
+                   DictionaryData date= DictionaryDataUtil.findById(branch.getCountry());
+                    if(date != null ){
+                        branch.setCountry(date.getName());
+                    }
                 }
             }
         }else{// 如果没有境外分支清除列表
@@ -2997,6 +3003,8 @@ public class ExpertController extends BaseController {
             //		    List < SupplierCertServe > listSupplierCertSes = new ArrayList < SupplierCertServe > ();
             if (supplier.getSupplierMatSe() != null && supplier.getSupplierMatSe().getListSupplierCertSes() != null&&supplier.getSupplierTypeIds().contains("SERVICE")) {
                 List < SupplierCertServe >    listSupplierCertSes = supplier.getSupplierMatSe().getListSupplierCertSes();
+               if(listSupplierCertSes!=null && !listSupplierCertSes.isEmpty()){
+
                 for(SupplierCertServe server: listSupplierCertSes) {
                 	if(server.getCode() != null){
                 		SupplierCertPro pro = new SupplierCertPro();
@@ -3010,10 +3018,12 @@ public class ExpertController extends BaseController {
                         listSupplierCertPros.add(pro);
                 	}
                 }
+               }
             }
             //		    List < SupplierCertSell > listSupplierCertSells = new ArrayList < SupplierCertSell > ();
             if (supplier.getSupplierMatSell() != null && supplier.getSupplierMatSell().getListSupplierCertSells() != null&&supplier.getSupplierTypeIds().contains("SALES")) {
                 List < SupplierCertSell >    listSupplierCertSells = supplier.getSupplierMatSell().getListSupplierCertSells();
+                if(listSupplierCertSells !=null && !listSupplierCertSells.isEmpty()){
                 for(SupplierCertSell sell: listSupplierCertSells) {
                 	if(sell.getCode() != null){
                 		SupplierCertPro pro = new SupplierCertPro();
@@ -3027,7 +3037,7 @@ public class ExpertController extends BaseController {
                         listSupplierCertPros.add(pro);
                 	}
                 }
-               
+                }
             }
             supplier.getSupplierMatPro().setListSupplierCertPros(listSupplierCertPros);
 
@@ -3035,28 +3045,30 @@ public class ExpertController extends BaseController {
         List < SupplierCateTree > allTreeList = new ArrayList < SupplierCateTree > ();
         //List < SupplierItem > itemsList = supplierItemService.findCategoryList(supplier.getId(), null, null);
         List < SupplierItem > itemsList = supplierItemService.getItemList(supplier.getId(), null, (byte)0, null);
-        for(SupplierItem supplierItem: itemsList) {
-            if(supplier.getSupplierTypeIds().contains(supplierItem.getSupplierTypeRelateId())){
-                SupplierCateTree cateTree = getTreeListByCategoryId(supplierItem);
-                if(cateTree != null && cateTree.getRootNode() != null) {
-                	//System.out.println(cateTree.getRootNode()+"============");
-                	switch (cateTree.getRootNode()) {
-					case "物资生产":
-						cateTree.setRootNodeType(1);
-						break;
-					case "物资销售":
-						cateTree.setRootNodeType(2);
-						break;
-					case "工程":
-						cateTree.setRootNodeType(3);
-						break;
-					case "服务":
-						cateTree.setRootNodeType(4);
-						break;
-					default:
-						break;
-					}
-                    allTreeList.add(cateTree);
+        if(itemsList !=null && !itemsList.isEmpty()) {
+            for (SupplierItem supplierItem : itemsList) {
+                if (supplier.getSupplierTypeIds().contains(supplierItem.getSupplierTypeRelateId())) {
+                    SupplierCateTree cateTree = getTreeListByCategoryId(supplierItem);
+                    if (cateTree != null && cateTree.getRootNode() != null) {
+                        //System.out.println(cateTree.getRootNode()+"============");
+                        switch (cateTree.getRootNode()) {
+                            case "物资生产":
+                                cateTree.setRootNodeType(1);
+                                break;
+                            case "物资销售":
+                                cateTree.setRootNodeType(2);
+                                break;
+                            case "工程":
+                                cateTree.setRootNodeType(3);
+                                break;
+                            case "服务":
+                                cateTree.setRootNodeType(4);
+                                break;
+                            default:
+                                break;
+                        }
+                        allTreeList.add(cateTree);
+                    }
                 }
             }
         }
@@ -3067,6 +3079,8 @@ public class ExpertController extends BaseController {
         // 工程类证书
         if (supplier.getIsEng() != null) {
             List<SupplierAptitute> listSupplierAptitutes = supplier.getSupplierMatEng().getListSupplierAptitutes();
+            if(listSupplierAptitutes !=null && !listSupplierAptitutes.isEmpty()){
+
             for (SupplierAptitute apt : listSupplierAptitutes) {
                 Qualification certType = qualificationService.getQualification(apt.getCertType());
                 if (certType != null) {
@@ -3076,6 +3090,8 @@ public class ExpertController extends BaseController {
                 if (aptituteLevel != null) {
                     apt.setAptituteLevel(aptituteLevel.getName());
                 }
+            }
+
             }
         }
         supplier.setAllTreeList(allTreeList);
