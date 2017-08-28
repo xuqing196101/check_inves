8b690d1f17b860a4cbb091b4d1e7b1135fd46a2b
mtl <yu513517@163.com>
2017-08-28 14:24:29 +0800

    【修改】【作者：马田力】删除无用的控制台输出

--- src/main/webapp/js/ses/ems/againAudit/batchDetails.js
+++ src/main/webapp/js/ses/ems/againAudit/batchDetails.js
@@ -10,7 +10,6 @@
       data: {},
       success: function (data) {
         list_content = data.object;  // 储存所需数据到变量
-        console.log(list_content);
         $('#list_content').html('');
         for (var i in list_content.list) {
           // 判断状态输出
