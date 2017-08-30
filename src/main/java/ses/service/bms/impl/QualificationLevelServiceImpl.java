d51d374b65a140f28ddec72d816b9bc3d45a1898
githubhxg <1059056687@qq.com>
2017-08-30 19:51:19 +0800

    【修改】供应商注册--工程专业信息资质类型和资质等级选择和输入的控制【作者】黄习刚

--- src/main/java/ses/service/bms/impl/QualificationLevelServiceImpl.java
+++ src/main/java/ses/service/bms/impl/QualificationLevelServiceImpl.java
@@ -43,9 +43,9 @@ public class QualificationLevelServiceImpl  implements QualificationLevelService
 		
 	}
 	@Override
-	public List<DictionaryData> getByQuaId(String id) {
+	public List<DictionaryData> getByQuaId(String quaId) {
 		List<DictionaryData> lis1=new ArrayList<DictionaryData>();
-		List<QualificationLevel> list = qualificationLevelMapper.findList(id);
+		List<QualificationLevel> list = qualificationLevelMapper.findList(quaId);
 		for(QualificationLevel q:list){
 			DictionaryData data = DictionaryDataUtil.findById(q.getGrade());
 			lis1.add(data);
@@ -66,5 +66,9 @@ public class QualificationLevelServiceImpl  implements QualificationLevelService
 	public void deleteByQuaId(String quaId) {
 		 
 		qualificationLevelMapper.delete(quaId);
+	}
+	@Override
+	public int countByQuaIdAndLevel(String quaId, String level) {
+		return qualificationLevelMapper.countByQuaIdAndLevel(quaId, level);
 	}
 }
