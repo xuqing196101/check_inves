c5725c2974fc0465465baae743c0cea185fa0234
17319170452 <17319170452@163.com>
2017-09-13 11:30:12 +0800

    Revert "【修改】恢复原抽取功能controller【作者】贾成祥"
    This reverts commit 3d744dfb85228e17b653d9a30b64c193f55307e2.

--- src/main/java/ses/controller/sys/sms/SupplierExtractsController.java
+++ src/main/java/ses/controller/sys/sms/SupplierExtractsController.java
@@ -78,7 +78,7 @@ import common.annotation.CurrentUser;
  */
 @Controller
 @Scope("prototype")
-@RequestMapping("/SupplierExtracts")
+@RequestMapping("/SupplierExtractsll")
 public class SupplierExtractsController extends BaseController {
     /**
      * 项目
