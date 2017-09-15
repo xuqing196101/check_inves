9647a299f67cec882d3ee2503b1772255fa02603
mtl <yu513517@163.com>
2017-09-15 17:27:01 +0800

    【修改】【作者：马田力】配置审核组成员重构

--- src/main/webapp/js/ses/ems/againAudit/editMembers.js
+++ src/main/webapp/js/ses/ems/againAudit/editMembers.js
@@ -12,37 +12,39 @@
         if (data.status) {
           list_content = data.object;  // 储存所需数据到变量
           
-          // 判断是否显示密码已设置信息
-          if (list_content.password === 0) {
-            $('#pwd_msg').html('密码未设置');
-          } else {
-            $('#set_newPassword').prop('disabled', true);
-          }
-          
-          // 判断用户名是否已有
-          if (list_content.userName != '') {
-            $('[name=loginName]').prop('disabled', true);
-            $('[name=loginName]').val(list_content.userName);
-          }
-          
-          if (typeof(list_content) != 'undefined') {
-            $('#list_content').html('');
-            for (var i in list_content.list.list) {
-              if (typeof(list_content.list.list[i].relName) === 'undefined') {
-                list_content.list.list[i].relName = '';
+          if (list_content.list.list.length > 0) {
+            // 判断是否显示密码已设置信息
+            if (list_content.password === 0) {
+              $('#pwd_msg').html('密码未设置');
+            } else {
+              $('#set_newPassword').prop('disabled', true);
+            }
+            
+            // 判断用户名是否已有
+            if (list_content.userName != '') {
+              $('[name=loginName]').prop('disabled', true);
+              $('[name=loginName]').val(list_content.userName);
+            }
+            
+            if (typeof(list_content) != 'undefined') {
+              $('#list_content').html('');
+              for (var i in list_content.list.list) {
+                if (typeof(list_content.list.list[i].relName) === 'undefined') {
+                  list_content.list.list[i].relName = '';
+                }
+                if (typeof(list_content.list.list[i].orgName) === 'undefined') {
+                  list_content.list.list[i].orgName = '';
+                }
+                if (typeof(list_content.list.list[i].duties) === 'undefined') {
+                  list_content.list.list[i].duties = '';
+                }
+                
+                $('#list_content').append('<tr>'
+                  +'<td class="text-center">'+ list_content.list.list[i].relName +'</td>'
+                  +'<td class="text-center">'+ list_content.list.list[i].orgName +'</td>'
+                  +'<td class="text-center">'+ list_content.list.list[i].duties +'</td>'
+                +'</tr>');
               }
-              if (typeof(list_content.list.list[i].orgName) === 'undefined') {
-                list_content.list.list[i].orgName = '';
-              }
-              if (typeof(list_content.list.list[i].duties) === 'undefined') {
-                list_content.list.list[i].duties = '';
-              }
-              
-              $('#list_content').append('<tr>'
-                +'<td class="text-center">'+ list_content.list.list[i].relName +'</td>'
-                +'<td class="text-center">'+ list_content.list.list[i].orgName +'</td>'
-                +'<td class="text-center">'+ list_content.list.list[i].duties +'</td>'
-              +'</tr>');
             }
           }
         } else {
