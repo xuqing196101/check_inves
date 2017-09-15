0c5d7da55211219017d2f7ab16a7de27af1bd03d
zhangshubin123 <1042884980@qq.com>
2017-09-15 15:50:13 +0800

    【添加】获取当前登录用户的session信息

---
+++ src/main/java/common/controller/GetSessionController.java
@@ -0,0 +1,47 @@
+package common.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alibaba.fastjson.JSON;
+
+import ses.model.bms.User;
+import ses.util.SessionListener;
+import common.annotation.CurrentUser;
+
+/**
+ * 
+ * Description: 获取当前登录的session
+ * 
+ * @author zhang shubin
+ * @version 2016-9-7
+ * @since JDK1.7
+ */
+@Controller
+@RequestMapping("/getSession")
+public class GetSessionController {
+
+	/**
+	 * 
+	 * Description: 获取到当前登录人的id和sessionMap
+	 * 
+	 * @author zhang shubin
+	 * @data 2017年9月15日
+	 * @param 
+	 * @return
+	 */
+	@RequestMapping("/getSessionMap")
+	public String getSessionMap(@CurrentUser User user){
+		Map<String, Object> map = new HashMap<String, Object>();
+		if(user != null){
+			map.put("userId", user.getId());
+		}else{
+			map.put("userId", "");
+		}
+		map.put("sessionMap", SessionListener.sessionMap);
+		return JSON.toJSONString(map);
+	}
+}
