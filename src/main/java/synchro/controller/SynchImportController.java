0dc69a4acb21ef61dc07e74f42835308020000bd
17319170452 <17319170452@163.com>
2017-11-06 11:08:19 +0800

    【修改】去除手动导入导出供应商抽取信息【作者】贾成祥

--- src/main/java/synchro/controller/SynchImportController.java
+++ src/main/java/synchro/controller/SynchImportController.java
@@ -203,14 +203,14 @@ public class SynchImportController {
                     continue;
                 }
                 // 过滤供应商抽取信息  定时任务自动导入导出
-               /* if (dd.getCode().equals(Constant.DATE_SYNCH_SUPPLIER_EXTRACT_INFO)) {
+                if (dd.getCode().equals(Constant.DATE_SYNCH_SUPPLIER_EXTRACT_INFO)) {
                 	iter.remove();
                 	continue;
                 }
                 if (dd.getCode().equals(Constant.DATE_SYNCH_SUPPLIER_EXTRACT_RESULT)) {
                 	iter.remove();
                 	continue;
-                }*/
+                }
                 //过滤军队专家信息
                 if (dd.getCode().equals(Constant.DATE_SYNCH_MILITARY_EXPERT)) {
                     iter.remove();
