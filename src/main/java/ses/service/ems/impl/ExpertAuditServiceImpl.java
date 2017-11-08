80652535b3dfa67ddcaae478b66709b605a41eab
xuqing <xuqing196101@163.com>
2017-11-08 14:10:33 +0800

    【修改】XJ-YH-51396-专家复审--复审--点暂存，提示暂存失败

--- src/main/java/ses/service/ems/impl/ExpertAuditServiceImpl.java
+++ src/main/java/ses/service/ems/impl/ExpertAuditServiceImpl.java
@@ -441,7 +441,7 @@ public class ExpertAuditServiceImpl implements ExpertAuditService {
 		}
 		Expert expertInfo = expertMapper.selectByPrimaryKey(expertId);
 		String status = expertInfo.getStatus();
-		if("0".equals(status) || "15".equals(status) || "16".equals(status) || "9".equals(status)){
+		if("0".equals(status) || "15".equals(status) || "16".equals(status) || "9".equals(status) || "-2".equals(status) || (sign==2 && "6".equals(status))){
 			//初审中
 			expert.setAuditTemporary(1);
 		}else if("4".equals(status)){
