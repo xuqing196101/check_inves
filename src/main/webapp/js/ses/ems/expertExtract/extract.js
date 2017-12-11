b546f4df679344d6f55d62ca1fbb2115c932446d
zhangshubin123 <1042884980@qq.com>
2017-12-11 18:36:46 +0800

    【修改】XJ-YH-55580--专家抽取--含有错误提示语提示时还自动带出请完善信息弹框，应只显示错误提示语

--- src/main/webapp/js/ses/ems/expertExtract/extract.js
+++ src/main/webapp/js/ses/ems/expertExtract/extract.js
@@ -152,7 +152,31 @@ function validationIsNull(code){
     //项目编号
     var xmProjectId = $("#xmProjectId").val();
     if(xmProjectId == null || xmProjectId == ''){
-    	vaCode(2);
+        var projectCode = $("#projectCode").val();
+        if(projectCode == null || projectCode == ""){
+            $("#err_code").html("项目编号不能为空");
+        }else{
+            // 验证项目编号重复校验
+            $.ajax({
+                url : globalPath + "/extractExpert/vaProjectCode.do",
+                data : {
+                    "code" : projectCode,
+                    "xmProjectId" : xmProjectId
+                },
+                dataType : "json",
+                async : false,
+                type : "POST",
+                success : function(data) {
+                    if(data.status == "no"){
+                        $("#err_code").html("项目编号已被使用");
+                        flag = false;
+                        layer.msg("请完善项目信息");
+                    }else{
+                        $("#err_code").html("");
+                    }
+                }
+            });
+        }
     }
     // 评审时间
     var reviewTime = $("#reviewTime").val();
@@ -1394,7 +1418,7 @@ function uuid() {
 }
 
 //验证项目编号
-function vaCode(ff){
+function vaCode(){
     var projectCode = $("#projectCode").val();
     var xmProjectId = $("#xmProjectId").val();
     if(projectCode == null || projectCode == ""){
@@ -1413,10 +1437,6 @@ function vaCode(ff){
             success : function(data) {
                 if(data.status == "no"){
                     $("#err_code").html("项目编号已被使用");
-                    flag = false;
-                    if(ff == "2"){
-                    	layer.msg("请完善项目信息");
-                    }
                 }else{
                     $("#err_code").html("");
                 }
